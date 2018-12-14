73e8331796f772e0f79db126440a2152184be667
Błażej Sowa <bsowa123@gmail.com>
2018-12-14 16:09:05 +0100

    remove uv4l

--- pi-gen-overlay/stage3/01-sys-tweaks/00-run.sh
+++ pi-gen-overlay/stage3/01-sys-tweaks/00-run.sh
@@ -1,20 +1,12 @@
 #!/bin/bash -e
 
-install -m 644 files/etc/apt/sources.list.d/*.list "$ROOTFS_DIR"/etc/apt/sources.list.d/
-
-on_chroot <<-EOF
-	curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | apt-key add -
+on_chroot << EOF
+rm /etc/motd
+rm /etc/update-motd.d/10-uname
 EOF
 
-# echo "=>Setting timezone to Warsaw"
-# on_chroot <<-EOF
-# 	timedatectl set-timezone Europe/Warsaw
-#     dpkg-reconfigure -f noninteractive tzdata
-# EOF
+install -m 755 files/etc/turtleos_version "$ROOTFS_DIR"/etc/
+install -m 755 files/etc/update-motd.d/* "$ROOTFS_DIR"/etc/update-motd.d/
 
-on_chroot <<-EOF
-    apt update
-    apt-get -y install uv4l uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-webrtc
-EOF
-
-install -v -m 644 files/etc/uv4l/*.conf	"${ROOTFS_DIR}/etc/uv4l/"
+DATE=$(date +"%Y%m%d")
+sed -i "/BUILD_ID=/s/$/$DATE/" "$ROOTFS_DIR"/etc/turtleos_version
