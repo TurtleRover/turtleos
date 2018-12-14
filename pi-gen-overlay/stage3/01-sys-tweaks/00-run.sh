415a81e079f68fa2213d1aa9c480c536b8cef468
Błażej Sowa <bsowa123@gmail.com>
2018-12-14 21:20:41 +0100

    add raspberry pi camera support

--- pi-gen-overlay/stage3/01-sys-tweaks/00-run.sh
+++ pi-gen-overlay/stage3/01-sys-tweaks/00-run.sh
@@ -5,8 +5,9 @@ rm /etc/motd
 rm /etc/update-motd.d/10-uname
 EOF
 
-install -m 755 files/etc/turtleos_version "$ROOTFS_DIR"/etc/
-install -m 755 files/etc/update-motd.d/* "$ROOTFS_DIR"/etc/update-motd.d/
+install -m 644 files/etc/turtleos_version "$ROOTFS_DIR"/etc/
+install -m 644 files/etc/update-motd.d/* "$ROOTFS_DIR"/etc/update-motd.d/
+install -m 644 files/etc/modules-load.d/* "$ROOTFS_DIR"/etc/modules-load.d/
 
 DATE=$(date +"%Y%m%d")
 sed -i "/BUILD_ID=/s/$/$DATE/" "$ROOTFS_DIR"/etc/turtleos_version
